ed8e5e01577151f35b3193d124e68f538730726b
MaribelClaros <claross.maribel@gmail.com>
2019-07-03 20:28:49 -0300

    Subo php y css del perfil del usuario.

--- perfil_usuario.php
+++ perfil_usuario.php
@@ -1,46 +1,54 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Perfil</title>
-    <link rel="stylesheet" href="css/perfil_usuario.css">
-  </head>
-  <body>
-
-    <header class = "cabecera">
-      <nav class="navbar-ppal">
-        <ul>
-          <li><a href="inicio.php">Inicio</a></li>
-          <li class="producto">
-            <a href="#">Productos <!-- <i class="fas fa-sort-down"></i> -->
-            </a>
-          </li>
-          <li class="buscador">
-            <a href="#">
-              <span><i class="fa fa-search"></i></span>
-              <input class="inputs" type="search" id="search" placeholder="Buscar..." />
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <nav class = "navbar-login">
-        <ul>
-          <li>
-            <a href="#" class="carro"><i class="fas fa-shopping-cart"></i></a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="img/Perfil.jpg" alt="imagen perfil">
-              <i class="fas fa-sort-down"></i>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-    <!-- Logo -->
-    <?php require_once "_logo.php" ?>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/perfil_usuario.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Noto+Sans|Open+Sans|Ubuntu&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mi Perfil</title>
+</head>
+<body>
+  <header class = "cabecera">
+    <nav class="navbar-ppal">
+      <ul>
+        <li>
+          <!-- Logo -->
+          <?php require_once "_logo.php" ?>
+        </li>
+        <li><a href="inicio.php">Inicio</a></li>
+        <li class="producto">
+          <a href="#">Productos <!-- <i class="fas fa-sort-down"></i> --></a>
+        </li>
+        <li class="buscador">
+          <a href="#">
+            <span><i class="fa fa-search"></i></span>
+            <input class="inputs" type="search" id="search" placeholder="Buscar..." />
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <nav class = "navbar-login">
+      <ul>
+        <li>
+          <a href="#" class="carro"><i class="fas fa-shopping-cart"></i></a>
+        </li>
+        <li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="dropNavBar" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img src="img/Perfil.jpg" width="40" style="border-radius: 50%; background-color: #ffffff; padding: 3px;">
+						</a>
+						<div class="dropdown-menu" aria-labelledby="dropNavBar">
+							<a class="dropdown-item" href="perfil_usuario.php">Mi perfil</a>
+							<a class="dropdown-item" href="">Salir</a>
+						</div>
+					</li>
+      </ul>
+    </nav>
+  </header>
 
     <h2>Bienvenida Maribel</h2>
 
@@ -52,11 +60,11 @@
         <form class="formulario">
           <div>
             <label for="">Nombre:</label>
-            <input class="inputs" type="text" name="" value="Maribel">
+            <input class="inputs" type="text" name="nombre" value="Maribel">
           </div>
           <div>
             <label for="">Apellido:</label>
-            <input class="inputs" type="text" name="" value="Claros">
+            <input class="inputs" type="text" name="apellido" value="Claros">
           </div>
           <div>
             <label for="">Fecha de nacimiento:</label>
@@ -82,7 +90,7 @@
           </div>
           <div>
             <label for="email">Email:</label>
-            <input class="inputs" type="email" name="" value="email@example.com">
+            <input class="inputs" type="email" name="email" value="email@example.com">
           </div>
           <div>
             <label for="cambiarpass">Cambiar contraseña:</label>
