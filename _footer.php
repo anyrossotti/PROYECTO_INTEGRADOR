ce71e8665be71a48819e44d12dd03ac0fec7f239
MaribelClaros <claross.maribel@gmail.com>
2019-07-04 05:42:11 -0300

    Actualizo footer y logo

--- _footer.php
+++ _footer.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <footer class="main-footer">
-    <div class="iconos-sociales">
-    <p class="redes">Seguinos en las redes!</p>
-    <div class="redes-sociales">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<footer class="main-footer">
+  <div class="iconos-sociales">
+    <h4 class="redes">¡Seguinos en nuestras redes!</h4>
     <a href="#"> <i class="fab fa-facebook"></i></a>
-      <a href="https://www.instagram.com/lienis.s.design/" target="_blank"> <i class="fab fa-instagram"></i></a>
-    </div>
-    </div>
-    </footer>
-  </body>
-</html>
+    <a href="https://www.instagram.com/lienis.s.design/" target="_blank"> <i class="fab fa-instagram"></i></a>
+  </div>
+</footer>
--- _logo.php
+++ _logo.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-    <section class="containerLogo">
-      <img src="img/logo-lienis.png" alt="logotipo" class="logo">
-      <section class="slogan">
-        <h1>La vie est belle, vivre avec le style Lienis. </h1>
-      </section>
-    </section>
-
-  </body>
-</html>
+<section class="containerLogo">
+  <img src="img/logo-lienis.png" alt="logotipo" class="logo">
+  <section class="slogan">
+    <h1>La vie est belle, vivre avec le style Lienis. </h1>
+  </section>
+</section>
